8131ad469fd1e9954563f864128520543b459bb0
jinbeizame007 <jinbeizame007@gmail.com>
2025-01-04 17:18:16 +0900

    [add] DiscreteTransferFunction

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+pub struct DiscreteTransferFunction {
+    num: Vec<f64>,
+    den: Vec<f64>,
+    inputs: Vec<f64>,
+    outputs: Vec<f64>,
+}
+
+impl DiscreteTransferFunction {
+    pub fn new(num: Vec<f64>, den: Vec<f64>) -> Self {
+        let inputs = vec![0.0; num.len()];
+        let outputs = vec![0.0; den.len()];
+        Self { num, den, inputs, outputs }
+    }
+
+    pub fn step(&mut self, input: f64) -> f64 {
+        let mut output = 0.0;
+
+        self.num.rotate_right(1);
+        self.num[0] = input;
+        for i in 0..self.num.len() {
+            output += self.num[i] * self.inputs[i];
+        }
+
+        self.den.rotate_right(1);
+        for i in 1..self.den.len() {
+            output -= self.den[i] * self.outputs[i];
+        }
+        output /= self.den[0];
+        self.outputs[0] = output;
+
+        output
+    }
+}
