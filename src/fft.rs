966f93aab1ee78a3af48641b95f9397f957ddb61
jinbeizame007 <jinbeizame007@gmail.com>
2025-01-19 15:07:51 +0900

    [add] ifft()

--- src/fft.rs
+++ src/fft.rs
@@ -68,8 +68,83 @@ fn dft(
     }
 }
 
-fn reverse_bits(input: usize, width: usize) -> usize {
-    (0..width).fold(0, |acc, i| (acc << 1) | ((input >> i) & 1))
+pub fn ifft(x: &DVector<Complex<f64>>) -> DVector<Complex<f64>> {
+    let n = x.len();
+    if (n as f64).log2().fract() != 0.0 {
+        panic!("n must be a power of 2");
+    }
+
+    let w = DVector::from_vec(
+        (0..(n / 2))
+            .map(|i| Complex::new(0.0, -2.0 * (n - i) as f64 * PI / n as f64).exp())
+            .collect::<Vec<Complex<f64>>>(),
+    );
+
+    let mut x_in = DVector::<Complex<f64>>::zeros(n);
+    let mut x_out = DVector::<Complex<f64>>::zeros(n);
+
+    for i in 0..n {
+        x_in[i] = x[i];
+        x_out[i] = x[i];
+    }
+
+    for i in 0..(n / 2) {
+        x_out[i] = x_in[i] + x_in[i + n / 2];
+        x_out[i + n / 2] = w[i] * (x_in[i] - x_in[i + n / 2]);
+    }
+
+    idft(
+        &mut x_in.rows_mut(0, n / 2),
+        &mut x_out.rows_mut(0, n / 2),
+        &w.rows_with_step(0, w.len() / 2, 1),
+    );
+    idft(
+        &mut x_in.rows_mut(n / 2, n / 2),
+        &mut x_out.rows_mut(n / 2, n / 2),
+        &w.rows_with_step(0, w.len() / 2, 1),
+    );
+
+    let mut result = DVector::<Complex<f64>>::zeros(n);
+    if n.ilog2() % 2 == 0 {
+        for i in 0..n {
+            result[reverse_bits(i, n.ilog2() as usize)] = x_in[i];
+        }
+    } else {
+        for i in 0..n {
+            result[reverse_bits(i, n.ilog2() as usize)] = x_out[i];
+        }
+    }
+
+    result = result.map(|c| c / (n as f64));
+
+    result
+}
+
+fn idft(
+    x_out: &mut DVectorViewMut<Complex<f64>>,
+    x_in: &mut DVectorViewMut<Complex<f64>>,
+    w: &DVectorView<Complex<f64>, Dyn>,
+) {
+    let n = x_in.nrows();
+    if n <= 1 {
+        return;
+    }
+
+    for i in 0..(n / 2) {
+        x_out[i] = x_in[i] + x_in[i + n / 2];
+        x_out[i + n / 2] = w[i] * (x_in[i] - x_in[i + n / 2]);
+    }
+
+    idft(
+        &mut x_in.rows_mut(0, n / 2),
+        &mut x_out.rows_mut(0, n / 2),
+        &w.rows_with_step(0, w.len() / 2, 1),
+    );
+    idft(
+        &mut x_in.rows_mut(n / 2, n / 2),
+        &mut x_out.rows_mut(n / 2, n / 2),
+        &w.rows_with_step(0, w.len() / 2, 1),
+    );
 }
 
 pub fn fftfreq(n: usize, dt: f64) -> DVector<f64> {
@@ -86,10 +161,15 @@ pub fn fftfreq(n: usize, dt: f64) -> DVector<f64> {
     result
 }
 
+fn reverse_bits(input: usize, width: usize) -> usize {
+    (0..width).fold(0, |acc, i| (acc << 1) | ((input >> i) & 1))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use approx::assert_relative_eq;
+    use rand::Rng;
 
     use nalgebra::dvector;
 
@@ -190,6 +270,73 @@ mod tests {
         assert_relative_eq!(
             freq,
             dvector![0.0, 0.5, 1., 1.5, 2., -2.5, -2., -1.5, -1., -0.5]
+        );
+    }
+
+    #[test]
+    fn test_ifft_2() {
+        let spectrum = dvector![Complex::new(1.0, 0.0), Complex::new(2.0, 0.0)];
+        let signal = ifft(&spectrum);
+
+        let signal_real = signal.map(|c| c.re);
+        let signal_imag = signal.map(|c| c.im);
+
+        assert_relative_eq!(signal_real, dvector![1.5, -0.5]);
+        assert_relative_eq!(signal_imag, dvector![0.0, 0.0]);
+    }
+
+    #[test]
+    fn test_ifft_4() {
+        let spectrum = dvector![
+            Complex::new(1.0, 0.0),
+            Complex::new(2.0, 0.0),
+            Complex::new(3.0, 0.0),
+            Complex::new(4.0, 0.0)
+        ];
+        let signal = ifft(&spectrum);
+
+        let signal_real = signal.map(|c| c.re);
+        let signal_imag = signal.map(|c| c.im);
+
+        assert_relative_eq!(signal_real, dvector![2.5, -0.5, -0.5, -0.5]);
+        assert_relative_eq!(signal_imag, dvector![0.0, -0.5, 0.0, 0.5]);
+    }
+
+    #[test]
+    fn test_ifft_8() {
+        let spectrum = dvector![
+            Complex::new(1.0, 0.0),
+            Complex::new(2.0, 0.0),
+            Complex::new(3.0, 0.0),
+            Complex::new(4.0, 0.0),
+            Complex::new(5.0, 0.0),
+            Complex::new(6.0, 0.0),
+            Complex::new(7.0, 0.0),
+            Complex::new(8.0, 0.0)
+        ];
+        let signal = ifft(&spectrum);
+
+        let signal_real = signal.map(|c| c.re);
+        let signal_imag = signal.map(|c| c.im);
+
+        assert_relative_eq!(
+            signal_real,
+            dvector![4.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5, -0.5],
+            epsilon = 1e-15
+        );
+        assert_relative_eq!(
+            signal_imag,
+            dvector![
+                0.0,
+                -1.2071067811865475,
+                -0.5,
+                -0.20710678118654746,
+                0.0,
+                0.20710678118654746,
+                0.5,
+                1.2071067811865475
+            ],
+            epsilon = 1e-15
         );
     }
 }
