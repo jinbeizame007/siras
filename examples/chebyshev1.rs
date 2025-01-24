f3aefc88e28f7b0b48cb04ff53d454a3232e0a5d
jinbeizame007 <jinbeizame007@gmail.com>
2025-01-25 00:58:01 +0900

    [add] example for a high pass chebyshev1 filter

--- examples/chebyshev1.rs
+++ examples/chebyshev1.rs
@@ -5,7 +5,7 @@ use nalgebra::DVector;
 use plotters::prelude::*;
 
 extern crate siras;
-use siras::filter_design::chebyshev1;
+use siras::filter_design::{chebyshev1, digital_to_analog_cutoff, FilterType};
 
 fn plot(
     x: &DVector<f64>,
@@ -52,18 +52,35 @@ fn main() {
     let f0 = 10.0;
     let f1 = 100.0;
     let sample_frequency = 32000;
-    let cutoff_frequency = 90.0;
-    let ripple = 0.1;
     let order = 4;
+    let ripple = 0.1;
     let t = DVector::from_iterator(
-        sample_frequency + 1,
-        (0..=sample_frequency).map(|i| i as f64 / sample_frequency as f64),
+        sample_frequency,
+        (0..sample_frequency).map(|i| i as f64 / sample_frequency as f64),
     );
     let x =
         (2.0 * PI * f0 * t.clone()).map(|e| e.sin()) + (2.0 * PI * f1 * t.clone()).map(|e| e.sin());
 
-    let mut tf = chebyshev1(order, cutoff_frequency, ripple);
-    let y = tf.filtfilt(&x, &t);
+    let cutoff_frequency_high_pass = digital_to_analog_cutoff(98.0, sample_frequency as f64);
+    let cutoff_frequency_low_pass = digital_to_analog_cutoff(15.0, sample_frequency as f64);
+
+    let alpha = 0.5;
+    let signal_with_high_pass_filter = chebyshev1(
+        order,
+        cutoff_frequency_high_pass,
+        ripple,
+        FilterType::HighPass,
+    )
+    .to_discrete(1.0 / sample_frequency as f64, alpha)
+    .filtfilt(&x, &t);
+    let signal_with_low_pass_filter = chebyshev1(
+        order,
+        cutoff_frequency_low_pass,
+        ripple,
+        FilterType::LowPass,
+    )
+    .to_discrete(1.0 / sample_frequency as f64, alpha)
+    .filtfilt(&x, &t);
 
     let plot_dir = "examples/plots";
     if !std::path::Path::new(plot_dir).exists() {
@@ -82,10 +99,20 @@ fn main() {
     .unwrap();
     plot(
         &t,
-        &y,
+        &signal_with_low_pass_filter,
         (1200, 600),
-        &format!("{}/chebyshev1_with_filter.png", plot_dir),
-        "with filter",
+        &format!("{}/chebyshev1_with_low_pass_filter.png", plot_dir),
+        "with low pass filter",
+        "time",
+        "amplitude",
+    )
+    .unwrap();
+    plot(
+        &t,
+        &signal_with_high_pass_filter,
+        (1200, 600),
+        &format!("{}/chebyshev1_with_high_pass_filter.png", plot_dir),
+        "with high pass filter",
         "time",
         "amplitude",
     )
